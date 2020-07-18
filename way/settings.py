867be2b25ad84a3ffcebb96828d09f77a88a1c41
MaycolCuero <maicolcr97@hotmail.com>
2020-07-18 15:25:32 -0500

    preparando todo para pasar los archivos clonados del servidor de heroku a la carpeta de git

--- way/settings.py
+++ way/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'way.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.postgresql',
@@ -103,7 +103,7 @@ DATABASES = {
     )
 }
 
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
